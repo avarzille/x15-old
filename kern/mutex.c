c7d0f35617eb53429dc098e7b4dee352e1a772be
Agustina Arzille <avarzille@riseup.net>
2017-06-06 04:23:30 -1000

    kern/mutex.c: Reworked adaptive lock implementation

--- kern/mutex.c
+++ kern/mutex.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013-2017 Richard Braun.
+ * Copyright (c) 2017 Agustina Arzille.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,35 +27,40 @@
 
 #define MUTEX_FLAGS_ALL   (MUTEX_WAITERS | MUTEX_LOCKED)
 
-static struct thread*
+static struct thread *
 mutex_owner_thread(uintptr_t owner)
 {
-    return ((struct thread *)(owner & ~MUTEX_FLAGS_ALL));
+    return (struct thread *)(owner & ~MUTEX_FLAGS_ALL);
 }
-
-/* Atomically modify the owner of a mutex, so that:
+ 
+/* Modifies the owner word of a mutex so that:
  * - If it's zero: Set ourselves as the new owner.
- * - If it's nonzero: Add a reference to the it and set the contended bit. */
+ * - If it's nonzero:
+ *   - If the waiters bit is set, return.
+ *   - Otherwise, try to atomically set the lock and waiters bit. If we
+ *     succeed, add a reference to the owner thread, and clear the lock bit.
+ */
 
 static uintptr_t
-mutex_update_owner(struct mutex *mutex, uintptr_t self)
+mutex_owner_update(struct mutex *mutex, uintptr_t self)
 {
     uintptr_t owner, new_owner;
 
     for (;;) {
-        owner = atomic_load_acquire(&mutex->owner);
+        owner = atomic_load(&mutex->owner, ATOMIC_RELAXED);
 
-        if (owner & MUTEX_LOCKED) {
+        if (owner & MUTEX_WAITERS) {
+            return owner;
+        } else if (owner & MUTEX_LOCKED) {
+            cpu_pause();
             continue;
         }
 
         new_owner = owner == 0 ? self : owner | MUTEX_FLAGS_ALL;
         if (atomic_cas_acquire(&mutex->owner, owner, new_owner) == owner) {
             if (owner != 0) {
-                thread_ref(mutex_owner_thread(owner));
-                /* clear the lock bit */
-                atomic_store_release(&mutex->owner,
-                                     new_owner & ~MUTEX_LOCKED);
+                thread_ref((struct thread *)owner);
+                atomic_store_release(&mutex->owner, new_owner & ~MUTEX_LOCKED);
             }
 
             return owner;
@@ -62,24 +68,33 @@ mutex_update_owner(struct mutex *mutex, uintptr_t self)
     }
 }
 
+static inline bool
+mutex_owner_eq(struct mutex *mutex, uintptr_t owner)
+{
+    uintptr_t prev;
+
+    prev = atomic_load(&mutex->owner, ATOMIC_RELAXED);
+    owner |= prev & MUTEX_WAITERS;
+    return owner == (prev & ~MUTEX_LOCKED);
+}
+
 void
 mutex_lock_slow(struct mutex *mutex)
 {
     struct sleepq *sleepq;
     unsigned long flags;
-    uintptr_t owner, self;
+    uintptr_t owner, self, tmp;
 
     self = (uintptr_t)thread_self();
-    owner = mutex_update_owner(mutex, self);
+    owner = mutex_owner_update(mutex, self);
 
     if (owner == 0) {
         return;
     }
 
     for (;;) {
-        /* if the owner matches our expectation, and it's running, spin */
-        if ((atomic_load_acquire(&mutex->owner) | MUTEX_FLAGS_ALL) ==
-            (owner | MUTEX_FLAGS_ALL)) {
+        /* If the owner matches our expectation and it's running, spin */
+        if (mutex_owner_eq(mutex, owner)) {
             if (thread_is_running(mutex_owner_thread(owner))) {
                 continue;
             }
@@ -87,27 +102,39 @@ mutex_lock_slow(struct mutex *mutex)
             break;
         }
 
-        /* the owner changed - unref the old one and retry */
-        thread_unref(mutex_owner_thread(owner));
-        owner = mutex_update_owner(mutex, self);
+        /* Delay the call to 'thread_unref', since it can be a
+         * potentially expensive operation, and it puts that waiter
+         * at a disadvantage. */
 
+        tmp = mutex_owner_update(mutex, self);
+
+        if ((owner & MUTEX_WAITERS) == 0) {
+            /* remove the reference that we added. */
+            thread_unref(mutex_owner_thread(owner));
+        }
+
+        owner = tmp;
         if (owner == 0) {
             return;
         }
     }
 
-    /* spinning didn't work - sleep */
+    /* Spinning didn't work - sleep */
     sleepq = sleepq_lend(mutex, false, &flags);
 
     for (;;) {
-        if ((atomic_load_acquire(&mutex->owner) | MUTEX_FLAGS_ALL) ==
-            (owner | MUTEX_FLAGS_ALL)) {
+        if (mutex_owner_eq(mutex, owner)) {
             sleepq_wait(sleepq, "mutex");
             continue;
         }
 
-        thread_unref(mutex_owner_thread(owner));
-        owner = mutex_update_owner(mutex, self);
+        tmp = mutex_owner_update(mutex, self);
+
+        if ((owner & MUTEX_WAITERS) == 0) {
+            thread_unref(mutex_owner_thread(owner));
+        }
+
+        owner = tmp;
         if (owner == 0) {
             break;
         }
@@ -122,23 +149,21 @@ mutex_unlock_slow(struct mutex *mutex)
     struct sleepq *sleepq;
     unsigned long flags;
     uintptr_t owner;
-
+    
     for (;;) {
-        owner = atomic_load_acquire(&mutex->owner);
-
-        if (owner & MUTEX_LOCKED) {
+        owner = atomic_load_acquire(&mutex->owner) & ~MUTEX_LOCKED;
+        if (atomic_cas_release(&mutex->owner, owner, 0) != owner) {
+            cpu_pause();
             continue;
-        } else if (atomic_cas_acquire(&mutex->owner, owner,
-                                      owner | MUTEX_LOCKED) == owner) {
-            sleepq = sleepq_acquire(mutex, false, &flags);
-            if (sleepq != NULL) {
-                sleepq_signal(sleepq);
-                sleepq_release(sleepq, flags);
-            }
-
-            atomic_store_release(&mutex->owner, 0);
-            return;
         }
+        
+        sleepq = sleepq_acquire(mutex, false, &flags);
+        if (sleepq != NULL) {
+            sleepq_signal(sleepq);
+            sleepq_release(sleepq, flags);
+        }
+        
+        return;
     }
 }
 
