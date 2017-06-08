e3052633bcfb3ae5915868e80a1c66cd37bbc178
Agustina Arzille <avarzille@riseup.net>
2017-06-08 06:00:14 -1000

    kern/mutex: Reimplementation without thread referencing.

--- kern/mutex.c
+++ kern/mutex.c
@@ -25,36 +25,31 @@
 #include <kern/mutex_i.h>
 #include <kern/sleepq.h>
 
-#define MUTEX_FLAGS_ALL   (MUTEX_WAITERS | MUTEX_INTLOCK)
-
 static struct thread *
 mutex_owner_thread(uintptr_t owner)
 {
-    return (struct thread *)(owner & ~MUTEX_FLAGS_ALL);
+    return (struct thread *)(owner & ~MUTEX_WAITERS);
 }
 
 static uintptr_t
-mutex_owner_update(struct mutex *mutex, uintptr_t self, uintptr_t ilb)
+mutex_owner_update(struct mutex *mutex, uintptr_t self)
 {
     uintptr_t owner, new_owner;
 
     for (;;) {
         owner = atomic_load(&mutex->owner, ATOMIC_RELAXED);
-
         if (owner & MUTEX_WAITERS) {
             return owner;
         }
 
-        new_owner = owner == 0 ? self : (owner | MUTEX_WAITERS | ilb);
-        if (atomic_cas_acquire(&mutex->owner, owner, new_owner) == owner) {
-            if (owner != 0 && ilb != 0) {
-                thread_ref((struct thread *)owner);
-                atomic_store_release(&mutex->owner,
-                                     new_owner & ~MUTEX_INTLOCK);
-            }
+        new_owner = owner <= MUTEX_FORCE_WAIT ?
+          self : (owner | MUTEX_WAITERS);
 
+        if (atomic_cas_acquire(&mutex->owner, owner, new_owner) == owner) {
             return owner;
         }
+
+        cpu_pause();
     }
 }
 
@@ -63,85 +58,40 @@ mutex_owner_eq(struct mutex *mutex, uintptr_t owner)
 {
     uintptr_t prev;
 
-    prev = atomic_load(&mutex->owner, ATOMIC_RELAXED) & ~MUTEX_FLAGS_ALL;
-    return prev == (owner & ~MUTEX_FLAGS_ALL);
+    prev = atomic_load(&mutex->owner, ATOMIC_RELAXED) & ~MUTEX_WAITERS;
+    return prev == (owner & ~MUTEX_WAITERS);
 }
 
-static bool
-mutex_try_spin(struct mutex *mutex, uintptr_t self, uintptr_t *prevp)
+void mutex_lock_slow(struct mutex *mutex)
 {
-    uintptr_t owner, tmp;
-    bool ret;
+    uintptr_t self, owner;
+    struct sleepq *sleepq;
+    unsigned long flags;
 
-    owner = mutex_owner_update(mutex, self, MUTEX_INTLOCK);
-    ret = true;
+    self = (uintptr_t)thread_self();
+    sleepq = sleepq_lend(mutex, false, &flags);
+    owner = mutex_owner_update(mutex, self);
 
-    if (owner == 0) {
-        return ret;
+    if (owner <= MUTEX_FORCE_WAIT) {
+        goto done;
     }
-
-    thread_preempt_disable();
 
     for (;;) {
         if (mutex_owner_eq(mutex, owner)) {
             if (!thread_is_running(mutex_owner_thread(owner))) {
-                *prevp = owner;
-                ret = false;
-                break;
+                sleepq_wait(sleepq, "mutex");
             }
 
             continue;
         }
 
-        tmp = mutex_owner_update(mutex, self, MUTEX_INTLOCK);
-
-        if ((owner & MUTEX_WAITERS) == 0) {
-            thread_unref(mutex_owner_thread(owner));
-        }
-
-        owner = tmp;
-        if (owner == 0) {
+        owner = mutex_owner_update(mutex, self | MUTEX_WAITERS);
+        if (owner <= MUTEX_FORCE_WAIT) {
             break;
         }
     }
 
-    thread_preempt_enable();
-    return ret;
-}
-
-void
-mutex_lock_slow(struct mutex *mutex)
-{
-    struct sleepq *sleepq;
-    unsigned long flags;
-    uintptr_t owner, self, tmp;
-
-    self = (uintptr_t)thread_self();
-
-    if (mutex_try_spin(mutex, self, &owner)) {
-        return;
-    }
-
-    sleepq = sleepq_lend(mutex, false, &flags);
-    tmp = mutex_owner_update(mutex, self, 0);
-
-    if ((owner & MUTEX_WAITERS) == 0) {
-        thread_unref((struct thread *)owner);
-    }
-
-    if (tmp == 0) {
-        sleepq_return(sleepq, flags);
-        return;
-    }
-
-    for (;;) {
-        if (mutex_owner_update(mutex, self, 0) == 0) {
-            break;
-        }
-
-        sleepq_wait(sleepq, "mutex");
-    }
-
+done:
     if (sleepq_empty(sleepq)) {
         atomic_store_release(&mutex->owner, self & ~MUTEX_WAITERS);
     }
@@ -154,23 +104,15 @@ mutex_unlock_slow(struct mutex *mutex)
 {
     struct sleepq *sleepq;
     unsigned long flags;
-    uintptr_t owner;
-    
-    for (;;) {
-        owner = atomic_load_acquire(&mutex->owner) & ~MUTEX_INTLOCK;
-        if (atomic_cas_release(&mutex->owner, owner, 0) != owner) {
-            cpu_pause();
-            continue;
-        }
-        
-        sleepq = sleepq_acquire(mutex, false, &flags);
-        if (sleepq != NULL) {
-            sleepq_signal(sleepq);
-            sleepq_release(sleepq, flags);
-        }
-        
-        return;
+
+    atomic_store_release(&mutex->owner, MUTEX_FORCE_WAIT);
+    sleepq = sleepq_acquire(mutex, false, &flags);
+
+    if (sleepq != NULL) {
+        sleepq_signal(sleepq);
+        sleepq_release(sleepq, flags);
     }
 }
 
 #endif /* X15_MUTEX_PI */
+
